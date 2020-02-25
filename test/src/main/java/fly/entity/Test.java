1f85d2ee929d3b1cb8f75ceaa73140f83895b30a
FlyInWind <2518509078@qq.com>
2020-02-25 12:10:26 +0800

    optimize build.gradle for jdk8, add Test class

---
+++ test/src/main/java/fly/entity/Test.java
@@ -0,0 +1,15 @@
+package fly.entity;
+
+import fly.entity.copier.Entity2Copier;
+
+/**
+ * @author FlyInWind
+ */
+public class Test {
+    public static void main(String[] args) {
+        Entity1 e = new Entity1();
+        e.setInt1(1);
+        e.setStr1("a");
+        Entity2 entity2 = Entity2Copier.generateFrom(e);
+    }
+}
